2af5cc1b829eeac9fcc7ea4001e12cf8f53373d2
João Paulo <163700907+splocs@users.noreply.github.com>
2024-06-05 15:35:15 -0300

    Update App.py

--- App.py
+++ App.py
@@ -57,7 +57,10 @@ def exibir_info_empresa(info):
                 st.write(f"  **Ano de Nascimento:** {director.get('yearBorn', 'N/A')}")
         else:
             st.write("Nenhum diretor encontrado.")
-            
+    st.markdown("# !analise de risco")   
+    st.write(f"**Capitalização de mercado:** {info.get('auditRisk', 'N/A')}")
+
+    
     st.write(f"**Capitalização de mercado:** {info.get('marketCap', 'N/A')}")
     st.write(f"**Preço atual:** {info.get('currentPrice', 'N/A')}")
     st.write(f"**Alta/baixa de 52 semanas:** {info.get('fiftyTwoWeekHigh', 'N/A')} / {info.get('fiftyTwoWeekLow', 'N/A')}")
