b9a307d58a26198feb86b596b0ebb0e6928b1666
João Paulo <163700907+splocs@users.noreply.github.com>
2024-06-06 18:47:58 -0300

    ok

--- App.py
+++ App.py
@@ -19,14 +19,9 @@ st.set_page_config(
 
 # Função para formatar a data
 def formatar_data(data):
-    if isinstance(data, pd.Timestamp):
-        return data.strftime('%d-%m-%Y')
-    elif isinstance(data, str):
-        try:
-            return pd.to_datetime(data).strftime('%d-%m-%Y')
-        except ValueError:
-            return data
-    return data
+    if data is not None:
+        return pd.to_datetime(data, unit='s').strftime('%d-%m-%Y')
+    return 'N/A'
 
 # Função para pegar os dados das ações
 def pegar_dados_acoes():
@@ -37,17 +32,13 @@ def pegar_dados_acoes():
 def pegar_valores_online(sigla_acao):
     df = yf.download(sigla_acao, DATA_INICIO, DATA_FIM, progress=False)
     df.reset_index(inplace=True)
-    df['Date'] = df['Date'].apply(formatar_data)
     return df
 
 # Função para pegar as informações da empresa
 def pegar_info_empresa(sigla_acao):
     ticker = yf.Ticker(sigla_acao)
     info = ticker.info
-    dividendos = ticker.dividends
-    dividendos = dividendos.reset_index()
-    dividendos['Date'] = dividendos['Date'].apply(formatar_data)
-    return info, dividendos
+    return info, ticker
 
 # Função para exibir informações da empresa
 def exibir_info_empresa(info, dividendos):
@@ -59,7 +50,7 @@ def exibir_info_empresa(info, dividendos):
     st.write(f"**País:** {info.get('country', 'N/A')}")
     st.write(f"**CEP:** {info.get('zip', 'N/A')}")
     st.write(f"**Telefone:** {info.get('phone', 'N/A')}")
-    st.write(f"**Site:** {info.get('website', 'N/A')}")
+    st.write(f"**Site:** {info.get('website', 'N/A')}")      
     st.write(f"**Setor:** {info.get('sector', 'N/A')}")
     st.write(f"**Indústria:** {info.get('industry', 'N/A')}")
     st.write(f"Moeda financeira: {info.get('financialCurrency', 'N/A')}")
@@ -111,7 +102,7 @@ def exibir_info_empresa(info, dividendos):
     st.markdown("#### Dividendos") 
     st.write(f"**Taxa de dividendos:** {info.get('dividendRate', 'N/A')}")
     st.write(f"**Dividend Yield:** {info.get('dividendYield', 'N/A')}")
-    st.write(f"**Data do ex dividendos:** {formatar_data(info.get('exDividendDate', 'N/A'))}")
+    st.write(f"**Data do ex dividendos:** {info.get('exDividendDate', 'N/A')}")
     st.write(f"**Índice de pagamento:** {info.get('payoutRatio', 'N/A')}")
     st.write(f"**Rendimento médio de dividendos últimos cinco anos:** {info.get('fiveYearAvgDividendYield', 'N/A')}")
 
@@ -130,27 +121,13 @@ def exibir_info_empresa(info, dividendos):
     st.write(f"**Margens de lucro:** {info.get('profitMargins', 'N/A')}")
     st.write(f"**Valor contábil:** {info.get('bookValue', 'N/A')}")
     st.write(f"**Preço/Valor contábil:** {info.get('priceToBook', 'N/A')}")
-    st.write(f"**Fim do último ano fiscal:** {formatar_data(info.get('lastFiscalYearEnd', 'N/A'))}")
-    st.write(f"**Fim do próximo ano fiscal:** {formatar_data(info.get('nextFiscalYearEnd', 'N/A'))}")
-    st.write(f"**Trimestre mais recente:** {formatar_data(info.get('mostRecentQuarter', 'N/A'))}")
+    st.write(f"**Fim do último ano fiscal:** {info.get('lastFiscalYearEnd', 'N/A')}")
+    st.write(f"**Fim do próximo ano fiscal:** {info.get('nextFiscalYearEnd', 'N/A')}")
+    st.write(f"**Trimestre mais recente:** {info.get('mostRecentQuarter', 'N/A')}")
     st.write(f"**Crescimento trimestral dos lucros:** {info.get('earningsQuarterlyGrowth', 'N/A')}")
     st.write(f"**Lucro líquido comum:** {info.get('netIncomeToCommon', 'N/A')}")
     st.write(f"**EPS (Lucro por ação) em retrospecto:** {info.get('trailingEps', 'N/A')}")
     st.write(f"**EPS (Lucro por ação) projetado:** {info.get('forwardEps', 'N/A')}")
     st.write(f"**Último fator de divisão:** {info.get('lastSplitFactor', 'N/A')}")
-    st.write(f"**Última data de divisão:** {formatar_data(info.get('lastSplitDate', 'N/A'))}")
-
-# Pegar a lista de ações
-acoes = pegar_dados_acoes()
-
-# Filtros de seleção
-acao = st.selectbox('Selecione a Ação', acoes['sigla'])
-
-# Seleciona a ação
-DATA_INICIO = '2020-01-01'
-DATA_FIM = date.today().strftime('%Y-%m-%d')
-dados_acao = pegar_valores_online(acao)
-info, dividendos = pegar_info_empresa(acao)
-
-# Exibe informações da empresa
-exibir_info_empresa(info, dividendos)
+    st.write(f"**Última data de divisão:** {info.get('lastSplitDate', 'N/A')}")
+    st
