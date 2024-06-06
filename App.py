44f23de9d03580e93051aa9fa75b3241e5e22698
João Paulo <163700907+splocs@users.noreply.github.com>
2024-06-06 16:10:19 -0300

    ok

--- App.py
+++ App.py
@@ -94,7 +94,7 @@ def exibir_info_empresa(info):
 
     st.markdown("#### Volume") 
     st.write(f"**Volume médio:** {info.get('averageVolume', 'N/A')}")
-    st.write(f"**Volume médio últimos 10 dias:** {info.get('averageVolume60days', 'N/A')}")
+    st.write(f"**Volume médio últimos 10 dias:** {info.get('averageVolume10days', 'N/A')}")
    
 
     st.write(f"**Taxa de dividendos:** {info.get('dividendRate', 'N/A')}")
