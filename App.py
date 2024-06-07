6996157e19aa32e96a4e91cb3f67ff9e448d5b26
João Paulo <163700907+splocs@users.noreply.github.com>
2024-06-07 11:30:34 -0300

    ok

--- App.py
+++ App.py
@@ -114,14 +114,16 @@ def exibir_info_empresa(info, dividendos):
     st.write(f"**Rendimento médio de dividendos últimos cinco anos:** {info.get('fiveYearAvgDividendYield', 'N/A')}")
 
   
-   with st.expander("Histórico de Dividendos", expanded=False):
-    if not dividendos.empty:
-        st.dataframe(dividendos)
-        grafico = criar_grafico_dividendos(dividendos)
-        st.plotly_chart(grafico)
-    else:
-        st.write("Nenhum dividendo encontrado.")
+    # Exibindo o DataFrame de dividendos dentro de um expander
+    with st.expander("Histórico de Dividendos", expanded=False):
+        if not dividendos.empty:
+            st.dataframe(dividendos)
+            grafico = criar_grafico_dividendos(dividendos)
+            st.plotly_chart(grafico)
+        else:
+            st.write("Nenhum dividendo encontrado.")
 
+            
 
     st.write(f"**Beta:** {info.get('beta', 'N/A')}")
     st.write(f"**P/L (Preço/Lucro) em retrospecto:** {info.get('trailingPE', 'N/A')}")
